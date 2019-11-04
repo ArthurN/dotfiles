3f27f16f3cdc7f29a047e5ad8ed7786ef2e3d6de
Arthur Nisnevich <arthur.nisnevich@gmail.com>
2019-11-03 21:33:21 -0700

    rebase --preserve-merges => --rebase-merges

--- .bash/git-completion.bash
+++ .bash/git-completion.bash
@@ -1682,7 +1682,7 @@ _git_rebase ()
   --*)
     __gitcomp "
       --onto --merge --strategy --interactive
-      --preserve-merges --stat --no-stat
+      --rebase-merges --stat --no-stat
       --committer-date-is-author-date --ignore-date
       --ignore-whitespace --whitespace=
       --autosquash --fork-point --no-fork-point
